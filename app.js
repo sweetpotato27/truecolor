f652de1b2ce05b14cbbc5aa1f19af226a636375c
Dylan Matthews <dmatthews@builder.local>
2020-08-09 16:05:47 -0700

    users and posts route init

--- app.js
+++ app.js
@@ -2,11 +2,24 @@ const Mongoose = require('mongoose');
 const express = require("express");
 const app = express();
 const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const posts = require("./routes/api/posts");
+const bodyParser = require('body-parser');
+
+Mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connected to MongoDB successfully"))
+.catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+app.use("/api/users", users);
+app.use("/api/posts", posts);
+
 const port = process.env.PORT || 5000;
 
-Mongoose.connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
-  .catch((err) => console.log(err));
+//i am not sure what bodyparser does... my json that i send up is still looking like json -_-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("IT HAS CHANGED"));
 app.listen(port, () => console.log(`Server is running on port ${port}`));
---
+++ routes/api/posts.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/test", (req, res) => res.json({ msg: "this is the posts route" }));
+
+module.exports = router;
