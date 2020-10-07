dc22713c6ab5b55ff0c04de3fec262f9c74f1c39
Cameron Getty <cmrngetty@gmail.com>
2020-10-06 20:22:13 -0700

    updating navbar and css

--- frontend/src/components/posts/post_box.js
+++ frontend/src/components/posts/post_box.js
@@ -6,8 +6,8 @@ class PostBox extends React.Component {
     render() {
         return (
             <div className="post-box-div">
-                <h3>{this.props.user ? this.props.user : ""}</h3>
-                <h3>{this.props.date ? this.props.date.split("T")[0] : ""}</h3>
+                <h3 class="user">{this.props.user ? this.props.user : ""}</h3>
+                <h3 class="date">{this.props.date ? this.props.date.split("T")[0] : ""}</h3>
                 {/* should we have similar logic for the image tag? */}
                 {/* might make sense to have a simple logo as placeholder for unloadable imageUrls */}
                 <img className="image" src={this.props.imageUrl} alt=""></img>
