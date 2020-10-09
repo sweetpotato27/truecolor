3ca4e6119c2334231636b5990a8de68c1897ee17
sweetpotato27 <matthewsdb34@gmail.com>
2020-10-09 10:20:04 -0700

    add footer needs editing and design

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/routes_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
+import FooterContainer from './footer/footer';
 
 import PostContainer from './posts/posts_container';
 import MainPage from './main/main_page';
@@ -11,6 +12,7 @@ import LoginFormContainer from './session/login_form_container';
 import SignupFormcontainer from './session/signup_form_container';
 import ProfileContainer from './profile/profile_container';
 import PostComposeContainer from './posts/post_compose_container';
+import infoContainer from './website-information/info_container';
 
 // At this point, we should be thinking about 
 // our own project and which kinds of routes are 
@@ -27,7 +29,9 @@ const App = () => (
             <ProtectedRoute exact path="/posts" component={PostContainer} />
             <ProtectedRoute exact path="/profile" component={ProfileContainer} />
             <ProtectedRoute exact path="/new_post" component={PostComposeContainer} />
+            <ProtectedRoute exact path="/info" component={infoContainer} />
         </Switch>
+        <FooterContainer />
     </div>
 );
 
---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+class Footer extends React.Component {
+
+  render() {
+    return (
+      <div className="footer">
+        <div className="column">
+          <p>
+            True Color is a collaborative community blog featuring photographers
+            and artists.
+          </p>
+        </div>
+        <div className="column">
+          <p>Hello</p>
+        </div>
+        <div className="column">
+          <p>Hello</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
