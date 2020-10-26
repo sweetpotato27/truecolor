668d5da6b21daa18b5bb0d2642b8ca4cc408b53b
sweetpotato27 <matthewsdb34@gmail.com>
2020-10-26 13:44:41 -0700

    kinda lost rn

---
+++ frontend/src/components/posts/post_gallery.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PostGallery extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className={`gallery-img gallery-img__${this.props.imageUrl}`}
+                id={`${this.props.imageUrl}__${this.props.index}`}>
+                <img className="image gallery-image" src={this.props.image}></img>
+            </div>
+        )
+    }
+}
+
+export default PostGallery;
